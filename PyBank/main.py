49c0cdb85c198a3cc9308f74f0dea860756448a0
ethomps33 <e.thomps33@gmail.com>
2021-01-14 21:56:55 -0700

    final code

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,43 +5,44 @@ budget_data = os.path.join('Resources', 'budget_data.csv')
 row_count = 0
 total_sum = 0
 total_change = 0
-largest_increase = 0
-largest_decrease = 0
+last_month_val = 0
+greatest_increase_difference = 0
+greatest_decrease_difference = 0
+greatest_increase_month = 0
+greatest_decrease_month = 0
 
-with open(budget_data) as csvfile:
+with open(budget_data, 'r') as csvfile:
     budget_csv = csv.reader(csvfile, delimiter = ',')
-    budget_header = next(budget_csv)
-    first_row=next(budget_csv)
-   
-    row_count+=1
     
-
+    first_row=next(budget_csv)
 
     for row in budget_csv:
         
-        last_month_val= int(first_row[1])
         if row_count != 0:
-            total_change += int(row[1]) - last_month_val
+            total_change = total_change + int(row[1]) - last_month_val
         row_count+=1
-        last_month_val= int(row[1])
-
         monthly_change= int(row[1]) - last_month_val
-        
-        total_change += monthly_change
+        if (monthly_change > greatest_increase_difference):
+            greatest_increase_difference = monthly_change
+            greatest_increase_month = row[0]
+
+        if (monthly_change < greatest_decrease_difference):
+            greatest_decrease_difference = monthly_change
+            greatest_decrease_month = row[0]
+
+        last_month_val= int(row[1])
        
-        last_month_val = int(row[1])
-        
-        largest_increase = max(largest_increase, monthly_change)
-        increase_date= row[0]
-        largest_decrease = min(largest_decrease, monthly_change)
-     total_sum+=int(row[1])
-        
+        total_sum+=int(row[1])        
+output_path = os.path.join("Analysis", "output.csv")
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    
     print("Finacial Analysis")
     print("--------------------------------")
     print(f'Total Months: {row_count}')
     print(f'Total: ${total_sum}')
-    print(f'Average Change: ${round((total_change/row_count), 2)}')
-    print(f'Greatest Increase in Profits: \n {increase_date}  ${largest_increase} ')
-    print(f'Greatest Decrease in Profits: \n  ${largest_decrease}') 
+    print(f'Average Change: ${round((total_change/(row_count-1)), 2)}')
+    print(f'Greatest Increase in Profits: \n {greatest_increase_month} (${greatest_increase_difference})')
+    print(f'Greatest Decrease in Profits: \n {greatest_decrease_month} (${greatest_decrease_difference})') 
 
         
